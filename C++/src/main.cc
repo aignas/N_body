2ebbaf291fd7ed9b8e2fcf1e55a7112af380de14
Ignas Anikevicius (gns_ank) <anikevicius@gmail.com>
2012-07-13 00:13:41 +0100

    Make the program do something useful
    Signed-off-by: Ignas Anikevicius (gns_ank) <anikevicius@gmail.com>

--- C++/src/main.cc
+++ C++/src/main.cc
@@ -8,9 +8,36 @@
 
 using namespace std;
 
+double Force ( double& G,
+               double& M,
+               double& m,
+               double r );
+
 int main (void)
 {
-    cout << "Hello World" << endl;
+    cout << "This program will calculate the Newtonian Force"
+         << endl;
+
+    double G = 1.0,
+           M = 5, 
+           m = 1;
+    double r = .5;
+
+    cout << "The value of G used in the calculations will be "
+         << G
+         << endl;
+
+    double force = Force(G,M,m,r);
+
+    cout << "The force is " << force << endl;
 
     return 0;
+}
+
+double Force ( double& G,
+               double& M,
+               double& m,
+               double r )
+{
+    return G*m*M/(r*r);
 }
