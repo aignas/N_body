6a2eb279760c554205e012f96d49c01b373c76a4
Ignas Anikevicius (gns_ank) <anikevicius@gmail.com>
2012-07-12 22:49:28 +0100

    Add a hello world program
    Signed-off-by: Ignas Anikevicius (gns_ank) <anikevicius@gmail.com>

---
+++ C++/src/main.cc
@@ -0,0 +1,16 @@
+// Written by Ignas Anikevicius in 2012
+// 
+// Should compile with g++
+//
+// Do whatever you want with the code.
+
+#include <iostream>
+
+using namespace std;
+
+int main (void)
+{
+    cout << "Hello World" << endl;
+
+    return 0;
+}
